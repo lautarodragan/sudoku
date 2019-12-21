191999172cf9f76c64ff6fe634659641d12cd23e
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-12-20 23:43:26 -0300

    cell selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import classnames from 'classnames'
+import React, { useState, useEffect } from 'react'
 
 import styles from './App.scss'
 
@@ -11,26 +12,41 @@ const setArrayValue = (array, position, value) => [...array.slice(0, position), 
 
 export const App = () => {
   const [board, setBoard] = useState(createMatrix())
+  const [selectedCell, setSelectedCell] = useState(null)
+
+  const onKeyPress = (event) => {
+    console.log('onKeyPress', event)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keypress', onKeyPress)
+    return () => document.removeEventListener('keypress', onKeyPress)
+  }, [])
 
   return (
     <section className={styles.app}>
       <header>Sudoku Score: 0</header>
-      <main><Board board={board} onClick={console.log} /></main>
+      <main><Board board={board} onClick={setSelectedCell} selectedCell={selectedCell} /></main>
       <footer>Sudoku by <a target="_blank" href="https://tarokun.io">tarokun</a></footer>
     </section>
   )
 }
 
-const Board = ({ board, onClick, onKeyPress }) => (
+const Board = ({ board, onClick, selectedCell }) => (
   <div className={styles.board}>
-    { board.map((row, rowIndex) => row.map((value, valueIndex) =>
-      <Cell key={rowIndex * 9 + valueIndex} value={value} onClick={_ => onClick(rowIndex, valueIndex)} />
+    { board.map((row, y) => row.map((value, x) =>
+      <Cell
+        key={y * 9 + x}
+        value={value}
+        onClick={_ => onClick({ x, y })}
+        isSelected={selectedCell && selectedCell.x === x && selectedCell.y === y}
+      />
     )) }
   </div>
 )
 
-const Cell = ({ value, onClick, onKeyPress }) => (
-  <div className={styles.cell} onClick={onClick} onKeyPress={onKeyPress}>
+const Cell = ({ value, onClick, isSelected }) => (
+  <div className={classnames(styles.cell, { [styles.selected]: isSelected })} onClick={onClick}>
     { value }
   </div>
 )
